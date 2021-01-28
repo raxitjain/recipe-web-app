03df57c6de6ecf3336b96816ec3c69d30578484c
Raxit Jain <rjain@nvenergy.com>
2021-01-28 14:06:36 +0530

    Initial project add

---
+++ src/test/java/com/ernstyoung/recipewebapp/RecipeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernstyoung.recipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
